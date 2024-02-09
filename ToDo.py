2ec3e7a7b23334804bf0750007a0dc93797eb339
RetrohUBZ1 <77767768+RetrohUBZ1@users.noreply.github.com>
2024-02-09 11:42:44 -0500

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,17 +1,11 @@
-#import numpy as np
 f = open("Tasklist.txt", 'r')
 task = []
-#complete = []
 
 def FileToArray():
     for a in f.readlines():
         task.append(a)
-    #if word = 'Line'
-#    for b in f.readlines():
- #       complete.append(b)
 
 def add_task(tasks):
-    #print("\n")
     task.append(tasks) #Adds input from user into the task array
     print("Task successfully added")
 
@@ -23,22 +17,6 @@ def view_task():
 
 def remove_task():
     try:
-        '''choice = int(input("\n Would you like to remove tasks from complete(1) or not complete(2)\n"))
-        if choice == 1:
-            if len(complete) > 0: #Checks to see if the length of the list has anything in it to begin with
-                for a in range(len(complete)):
-                    print(a, task[a])
-                    index = int(input("Which completed task would you like to remove?\n"))
-                    #if a in range(len(task)):
-                    task.pop(index)
-                    print("task successfully removed\n")
-                    #else:
-                     #   print("Line is out of bounds")
-            else:
-                print("No tasks to remove")
-
-        elif choice == 2:
-'''
         if len(task) > 0: #Checks to see if the length of the list has anything in it to begin with
               for b in range(len(task)):
                 print(b,task[b])
@@ -62,9 +40,6 @@ def complete_task():
     task[comp] = task[comp].strip() + ' COMPLETE\n'
     for a in task:
         print(a)
-    #task[comp] = newcomp
-    #complete.append(task[comp])
-    #task.pop(comp)
 
 
 def save_task():
@@ -74,10 +49,4 @@ def save_task():
         print(a, task[a])
     for line in task:
         save.writelines(f"{line}")
-    '''
-        save.writelines(task[a])
-    if len(complete) > 0:
-        for a in range(len(complete)):
-            save.writelines("\n Complete \n"+complete[a]+"\n")
-'''
     save.close()
